f0dbe7812182c78f32bf82a6cfb25cd5e800c418
nilkevich <anastasiya.ilkevich@chulakov.com>
2024-06-20 11:25:30 +0300

    fix styles

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ document.getElementById('imageUpload').addEventListener('change', function(event
 			reader.onload = function(e) {
 					const img = document.getElementById('uploadedImage');
 					img.src = e.target.result;
+					document.getElementById('imageContainer').classList.remove('hidden');
+					document.querySelector('.text-container').classList.remove('hidden');
 			}
 			reader.readAsDataURL(file);
 	}
 });
 
 document.getElementById('blurRange').addEventListener('input', function(event) {
-	const blurValue = event.target.value;
+	const blurPercent = event.target.value;
+	const maxBlurValue = 10; // Максимальное значение размытия в пикселях
+	const blurValue = (blurPercent / 100) * maxBlurValue;
 	const img = document.getElementById('uploadedImage');
 	img.style.filter = `blur(${blurValue}px)`;
+	document.getElementById('blurValue').textContent = blurPercent;
+	this.style.background = `linear-gradient(to right, #4CAF50 ${percentage}%, #ddd ${percentage}%)`;
 });
